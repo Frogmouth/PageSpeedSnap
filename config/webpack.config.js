29ed922a1b1d7ac661bdaabef97c633f7d27d1d2
Simone Luise <s.luise@accenture.com>
2025-06-13 12:20:44 +0200

    lint js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,22 +1,23 @@
-'use strict';
+"use strict";
 
-const { merge } = require('webpack-merge');
+const { merge } = require("webpack-merge");
 
-const common = require('./webpack.common.js');
-const PATHS = require('./paths');
+const common = require("./webpack.common.js");
+const PATHS = require("./paths");
 
 // Merge webpack configuration files
 const config = merge(common, {
   entry: {
     //react
-    popup: PATHS.src + '/js/popup.jsx',
-    dashboard: PATHS.src + '/js/dashboard.jsx',
-    settings: PATHS.src + '/js/settings.jsx',
-    background: PATHS.src + '/js/background.jsx',
+    popup: PATHS.src + "/js/popup.jsx",
+    dashboard: PATHS.src + "/js/dashboard.jsx",
+    settings: PATHS.src + "/js/settings.jsx",
+    background: PATHS.src + "/js/background.jsx",
+    mutator: PATHS.src + "/js/mutator.jsx",
 
     //javascript
-    contentScript: PATHS.src + '/js/contentScript.js',
-    injected: PATHS.src + '/js/injected.js',
+    contentScript: PATHS.src + "/js/contentScript.js",
+    injected: PATHS.src + "/js/injected.js",
   },
 });
 
